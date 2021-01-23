b41a0697ff43789586dc3dab1536bb82ef6defa9
msbilgin <msbilgin383@gmail.com>
2021-01-23 16:25:10 +0300

    Dictionary

---
+++ Dictionary/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyDictionary<string, int> Kadro = new MyDictionary<string, int>();
+            Kadro.Add("Atiba Hutchinson", 13);
+            Kadro.Add("Josef de Souza", 5);
+            Kadro.Add("Rıdvan Yılmaz", 33);
+            Kadro.Add("Cyle Larin", 17);
+
+            for (int i = 0; i < Kadro.key.Length; i++)
+            {
+                Console.WriteLine(Kadro.key[i] + " " + Kadro.value[i] + " ");
+            }
+        }
+    }
+}
